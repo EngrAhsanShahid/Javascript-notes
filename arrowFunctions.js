df5de1cd7e32252939a8d506f6817a84dd178279
EngrAhsanShahid <EngrAhsanShahid@users.noreply.github.com>
2023-12-01 18:49:01 +0500

    Create arrowFunctions.js

---
+++ arrowFunctions.js
@@ -0,0 +1,10 @@
+// const users = {
+//     name: "Ahsan Shahid",
+//     gender: "Male",
+//     welcomeMessage: function () {
+//         console.log(`Welcome ${this.name}`)
+//     }
+// }
+// users.welcomeMessage()
+// users.name = "Shah"
+// users.welcomeMessage()
